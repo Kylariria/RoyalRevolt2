1afd677664c77695a95854e0ae28a75def96fb66
Luoma <vokinne@gmail.com>
2024-02-12 14:32:41 +0100

    inchallah

--- RoyalRevolt2/CameraManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "CameraManager.h"
-#include "Macro.h"
-
-void CameraManager::Update(RenderWindow _window, const Vector2f& _position)
-{
-	CameraData* _camera = GetCurrent();
-	_camera->SetCenter(_position);
-}
