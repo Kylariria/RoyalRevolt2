7350a71c4dbff43f3eeab0ad8adc87cd3d9ad7f5
Kylariria <iriapaquis.pro@gmail.com>
2024-02-12 12:08:37 +0100

    eNTITY

---
+++ RoyalRevolt/EntityType.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum EntityType
+{
+    ENTITY_NONE,
+    ENTITY_SOLDAT,
+    
+};
---
+++ RoyalRevolt/Macro.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+#define S_ID(name) name + to_string(GetUniqueID())
+
+static int GetUniqueID()
+{
+	static int _id = 0;
+	return _id++;
+}
+
+static int Random(const int _max, const int _min = 0)
+{
+	return rand() % _max + _min;
+}
