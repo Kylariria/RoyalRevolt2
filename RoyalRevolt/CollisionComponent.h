1bc107a175cb98271f89ab91132c6ebc10008972
Luoma <vokinne@gmail.com>
2024-02-12 13:51:31 +0100

    added Player, InputManager, CollisionComponent

---
+++ RoyalRevolt/CollisionComponent.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "EntityType.h"
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <functional>
+
+using namespace std;
+using namespace sf;
+
+class Entity;
+
+
+struct CollisionReaction
+{
+	EntityType type;
+
+	function<void(Shape* _shape)> callbacks;
+
+	CollisionReaction(const EntityType& _type, const function<void(Shape*)>& _callbacks)
+	{
+		type = _type;
+		callbacks = _callbacks;
+	}
+};
+
+class CollisionComponent
+{
+	Shape* currentShape;
+
+public:
+	bool CheckCollision(Entity* _currentEntity, const vector<Entity*>& _allEntities, const vector<CollisionReaction>& _reactions);
+};
