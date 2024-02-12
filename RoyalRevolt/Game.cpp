5937eec6e49ec50c4b67a21eb3540a42bd2fc7f5
Luoma <vokinne@gmail.com>
2024-02-12 12:06:09 +0100

    Include Game, Singleton, IManager, IManagable, TextureManager, TextureData, Timer & TimerManager

--- RoyalRevolt/Game.cpp
+++ RoyalRevolt/Game.cpp
@@ -1,1 +1,50 @@
 #include "Game.h"
+#include <iostream>
+
+Game::Game()
+{
+	windowSize = Vector2f(SREEN_HEIGHT, SCREEN_WIDTH);
+	name = "Royal Revolt";
+}
+
+void Game::Launch()
+{
+	Start();
+	Update();
+}
+
+void Game::Start()
+{
+	window.create(VideoMode(static_cast<unsigned>(windowSize.x), static_cast<unsigned>(windowSize.y)), name);
+}
+
+void Game::Update()
+{
+	while (window.isOpen())
+	{
+		UpdateEvent();
+	}
+}
+
+void Game::UpdateEvent()
+{
+	Event _event;
+
+	while (window.pollEvent(_event))
+	{
+		if (_event.type == Event::Closed) Stop();
+	}
+	UpdateWindow();
+}
+
+void Game::UpdateWindow()
+{
+	window.clear();
+	//window.draw();
+	window.display();
+}
+
+void Game::Stop()
+{
+	cout << "End of " << name << endl;
+}
---
+++ RoyalRevolt/Singleton.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename Class>
+class Singleton
+{
+protected:
+    static Class instance;
+
+public:
+    static Class& GetInstance()
+    {
+        return instance;
+    }
+
+public:
+    Singleton() {}
+};
+
+template <typename Class>
+Class Singleton<Class>::instance;
