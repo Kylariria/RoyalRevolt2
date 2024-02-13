364cb666cb7540da0d71f395b2770598027a9dd9
DamienP1602 <148540742+DamienP1602@users.noreply.github.com>
2024-02-13 11:55:22 +0100

    v2

--- RoyalRevolt/Hero.cpp
+++ RoyalRevolt/Hero.cpp
@@ -1,5 +1,5 @@
 #include "Hero.h"
 
-Hero::Hero(const string& _name, const EntityType _type, const Vector2f& _position, const Vector2f& _size, const string& _path) : Entity(EntityData(_name, _type, _position, _size, _path))
+Hero::Hero(const string& _name, const EntityType _type, const Vector2f& _position, const Vector2f& _size, const string& _path) : MovingEntity(EntityData(_name, _type, _position, _size,1, _path))
 {
 }
--- RoyalRevolt/Hero.h
+++ RoyalRevolt/Hero.h
@@ -1,10 +1,9 @@
 #pragma once
-#include "Entity.h"
+#include "MovingEntity.h"
 
-class Hero : public Entity
+class Hero : public MovingEntity
 {
 
 public:
 	Hero(const string& _name, const EntityType _type, const Vector2f& _position, const Vector2f& _size, const string& _path);
 };
-
