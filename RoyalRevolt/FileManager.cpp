07f6322a7516658603946896d1e058f8c29b2ea7
Lefleur05 <nic2005.nb@gmail.com>
2024-02-13 11:01:57 +0100

    FileManager continute

--- RoyalRevolt/FileManager.cpp
+++ RoyalRevolt/FileManager.cpp
@@ -1,33 +1,52 @@
 #include "FileManager.h"
 #include <functional>
+#include <iostream>
+#include <fstream>
+#include "Tree.h"
+#include "Rock.h"
+#include "Path.h"
 
+#define PATH ""
+
+
+// path == #
+// Rock == R
+// Tree == T
 
 vector<Entity*> FileManager::CreateEntityFromChar(const string& _path)
 {
 	vector<Entity*> allEntities;
 
+	ifstream _stream(_path);
+	char _sign[] = { '#', 'R', 'T'};
+    if (!_stream)
+    {
+        cerr << "Erreur lors de l'ouverture du fichier " << _path << endl;
+        return vector<Entity*>();
+    }
 
+    float _posX = 0.f, _posY = 0.f;
+    string _line;
+    char _letter = '0';
 
-
-	
-
-	if (true)
-	{
-
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while (getline(_stream, _line))
+    {
+        for (int _index = 0; _index < _line.size(); _index++)
+        {
+            switch (_line[_index])
+            {
+            case '#':
+                CreateAndAddEntity<Path>("Path",Vector2f(_posX, _posY), PATH, Vector2f(50.0f, 50.0f));
+                break;
+            default:
+                break;
+            }
+            allEntities.back()->GetShape()->setPosition(_posX, _posY);
+            _posX += 1;
+        }
+        _posX = 0;
+        _posY += 1;
+    }
 
 
 
@@ -36,4 +55,29 @@ vector<Entity*> FileManager::CreateEntityFromChar(const string& _path)
 
 
     return vector<Entity*>();
+
 }
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
